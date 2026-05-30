33a6a70de47acf9172ade9675653e8869fa28146
Vijay Grandhi <118528505+vjy-07@users.noreply.github.com>
2026-05-30 16:46:41 +0530

    Added solution - LeetHub

---
+++ 705359-print-solid-square-GFG/705359-print-solid-square.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
