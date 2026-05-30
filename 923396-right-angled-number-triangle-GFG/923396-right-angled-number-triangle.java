6aaa43ee72b69de18b74291d05f0bad7ef4cd2c9
Vijay Grandhi <118528505+vjy-07@users.noreply.github.com>
2026-05-30 17:00:35 +0530

    Added solution - LeetHub

---
+++ 923396-right-angled-number-triangle-GFG/923396-right-angled-number-triangle.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void printRightTriangle(int n) {
+        // code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
