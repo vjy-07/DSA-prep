7ac8bda123789df9ae6662b4bf5a03e0f8573f25
Vijay Grandhi <118528505+vjy-07@users.noreply.github.com>
2026-05-30 16:51:43 +0530

    Added solution - LeetHub

---
+++ 878899-inverted-right-angle-triangle-GFG/878899-inverted-right-angle-triangle.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void printPattern(int n) {
+        // Code here
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
