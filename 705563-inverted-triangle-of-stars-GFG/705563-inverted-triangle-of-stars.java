51ffcf9daf0bb7a9fb6539332728d7b559ed58d6
Vijay Grandhi <118528505+vjy-07@users.noreply.github.com>
2026-05-30 16:53:51 +0530

    Added solution - LeetHub

---
+++ 705563-inverted-triangle-of-stars-GFG/705563-inverted-triangle-of-stars.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(j<i){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            for(int j=1;j<=n-i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
