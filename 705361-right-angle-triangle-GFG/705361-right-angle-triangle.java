ff3099a728e04731887bb1e6a90a5b5e90beb5b0
Vijay Grandhi <118528505+vjy-07@users.noreply.github.com>
2026-05-30 16:50:31 +0530

    Added solution - LeetHub

---
+++ 705361-right-angle-triangle-GFG/705361-right-angle-triangle.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+class Solution {
+    public static void triangleWall(int s) {
+        for(int i=1;i<=s;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
