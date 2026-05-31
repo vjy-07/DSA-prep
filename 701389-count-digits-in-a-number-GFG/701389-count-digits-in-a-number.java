da04086bc3218452ea6dc5b391eb1b8044328546
Vijay Grandhi <118528505+vjy-07@users.noreply.github.com>
2026-05-31 09:59:31 +0530

    Added solution - LeetHub

---
+++ 701389-count-digits-in-a-number-GFG/701389-count-digits-in-a-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int countDigits(int n) {
+        // Code here
+        return (int)Math.log10(n)+1;
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
