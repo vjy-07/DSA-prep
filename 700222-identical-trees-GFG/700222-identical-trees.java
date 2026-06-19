799b418a71e9d3a5cb7d3aabd2f51c2d314fab0a
Vijay Grandhi <118528505+vjy-07@users.noreply.github.com>
2026-06-19 14:21:13 +0530

    Added solution - LeetHub

---
+++ 700222-identical-trees-GFG/700222-identical-trees.java
@@ -0,0 +1,24 @@
+/*
+class Node{
+    int data;
+    Node left, right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+*/
+
+class Solution {
+    public boolean isIdentical(Node r1, Node r2) {
+        // code here
+        if(r1==null || r2==null) return  r1==r2;
+        return (r1.data == r2.data) && 
+                isIdentical(r1.left, r2.left) &&
+                isIdentical(r1.right, r2.right);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
