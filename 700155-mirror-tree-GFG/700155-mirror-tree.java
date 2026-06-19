10f152f8887ef1c9a885a4a5d5faaab05aae90b3
Vijay Grandhi <118528505+vjy-07@users.noreply.github.com>
2026-06-19 14:29:46 +0530

    Added solution - LeetHub

---
+++ 700155-mirror-tree-GFG/700155-mirror-tree.java
@@ -0,0 +1,28 @@
+/*
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+*/
+
+class Solution {
+    void mirror(Node root) {
+        // code here
+        if(root == null) return;
+        Node temp = root.left;
+        root.left= root.right;
+        root.right=temp;
+        mirror(root.left);
+        mirror(root.right);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
